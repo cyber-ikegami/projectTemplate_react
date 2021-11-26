5d2f6987067aee1a2e9dceab10b264340c1632bc
cyber-ikegami <89112341+cyber-ikegami@users.noreply.github.com>
2021-11-26 13:41:33 +0900

    進捗状況1126_01

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import styled from 'styled-components';
-//import logo from './logo.svg';
-import './App.css';
+// import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <_Test>Hello world</_Test>
-    </div>
+    <>
+      <_Form>
+        <textarea></textarea>
+      </_Form>
+
+      <_Buttons>
+        <button>抽出</button>
+        <button>リセット</button>
+      </_Buttons>
+
+      <_Form>
+        <textarea></textarea>
+      </_Form>
+    </>
   );
 }
 
@@ -17,4 +27,34 @@ const _Test = styled.div`
   color: #ffb700;
   font-size: 30px;
   font-weight: 600;
+`;
+
+const _Form = styled.div`
+  background-color: #b0ee97;
+  display: inline-block;
+  vertical-align: top;
+  width: calc(50% - 40px);
+  height: 100%;
+
+  & textarea {
+    margin-left: 10px;
+    margin-bottom: 10px;
+    width: calc(100% - 20px);
+    height: calc(100% - 30px);
+  }
+`;
+
+const _Buttons = styled.div`
+  background-color: #eed297;
+  display: inline-block;
+  vertical-align: top;
+  width: 80px;
+  height: 100%;
+  display: inline-flex;
+  flex-flow: column;
+  justify-content: center;
+
+  & button {
+    width: 100%;
+  }
 `;
